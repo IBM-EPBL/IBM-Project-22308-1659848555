4e69f816fdd777a778ca9d622aab792224cc8bfc
JeevikaD <jeevika.dp@gmail.com>
2022-09-25 21:19:54 +0530

    Assignment 2

---
+++ ASSIGNMENT/HARISH/Assignment 2/temp.py
@@ -0,0 +1,19 @@
+
+#Temperature and Humidity sensing using random
+
+
+import random
+import time
+while(1):
+    temp = random.uniform(15.0, 40.0)
+    humidity = random.randint(1, 100)
+    print("\nTemperature : {:.2f}*C   Humidity : {}%" .format(temp,humidity))
+    if temp > 30 :  #temp in celsius
+        if humidity > 80:
+            print("\n Alert: Abnormal condition predicted")
+        else:
+            print("\n Warning: Temperature is high")
+    else:
+        print("\n Normal Temperature")
+    time.sleep(1)    
+    
